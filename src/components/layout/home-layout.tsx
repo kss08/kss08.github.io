a5b403da42d2ba05355a2b24e3c24fe227c2100f
algebananazzzzz <algebananazzzzz.devops@gmail.com>
2025-09-30 18:16:22 +0800

    feat: dynamic contact buttons in navbar

--- src/components/layout/home-layout.tsx
+++ src/components/layout/home-layout.tsx
@@ -1,19 +1,42 @@
 import * as React from 'react';
 import { Link } from 'gatsby';
-
+import type { NavbarContent } from '@/types/navbar';
 
 const HomeLayout = ({ children }: { children: React.ReactNode }) => {
-    const title: string = "My Portfolio"
+    const content: NavbarContent = {
+        title: "My Portfolio",
+        contact_buttons: [
+            {
+                name: "github",
+                link: "github.com"
+            },
+            {
+                name: "linkedin",
+                link: "linkedin.com/in"
+            }
+        ]
+    }
 
     return <body className="flex flex-col min-h-screen">
         {/* Header */}
         <header className="bg-blue-600 text-white p-4">
-            <h1 className="text-2xl font-bold">{title}</h1>
-            <nav className="border-t pt-4 space-x-4">
-                <Link to="/">Home</Link>
-                <Link to="/skills">Skills</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/experiences">Experiences</Link>
+            <h1 className="text-2xl font-bold">{content.title}</h1>
+            <nav className="flex justify-between border-t pt-4">
+                <div className='space-x-4'>
+                    <Link to="/">Home</Link>
+                    <Link to="/skills">Skills</Link>
+                    <Link to="/projects">Projects</Link>
+                    <Link to="/experiences">Experiences</Link>
+                </div>
+                <div className='space-x-4'>
+                    {content.contact_buttons.map((button) => (
+                        <a
+                            href={button.link}
+                            target='blank'>
+                            {button.name}
+                        </a>
+                    ))}
+                </div>
             </nav>
         </header>
 
